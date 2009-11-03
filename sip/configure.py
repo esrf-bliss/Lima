1aedbaa1e9f913e0df5d5105a129ffb47a7378a2
ahoms <ahoms@45c4679d-1946-429d-baad-37912b19538b>
2009-11-03 14:44:39 +0000

    * added -g compiler flag for including debug info in binaries
    git-svn-id: https://scm.blissgarden.org/svn/lima/trunk@173 45c4679d-1946-429d-baad-37912b19538b

--- sip/configure.py
+++ sip/configure.py
@@ -71,7 +71,7 @@ makefile = sipconfig.ModuleMakefile(
 makefile.extra_include_dirs = extra_includes
 makefile.extra_libs = ['pthread','lima']
 makefile.extra_lib_dirs = ['../build']
-makefile.extra_cxxflags = ['-pthread']
+makefile.extra_cxxflags = ['-pthread', '-g']
 makefile.extra_cxxflags.extend(['-I' + x for x in extra_includes])
 # Add the library we are wrapping.  The name doesn't include any platform
 # specific prefixes or extensions (e.g. the "lib" prefix on UNIX, or the
