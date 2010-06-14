5c2555a852da6ddbf1dda16e69548195c457b57b
ahoms <ahoms@45c4679d-1946-429d-baad-37912b19538b>
2010-06-14 08:59:02 +0000

    * adapted to dynamic structure (before rename ...)
    git-svn-id: https://scm.blissgarden.org/svn/lima/trunk@256 45c4679d-1946-429d-baad-37912b19538b

--- sip/clean.py
+++ sip/clean.py
@@ -2,12 +2,31 @@
 
 import os
 import os.path
-if os.access('./Makefile',os.R_OK) :
-    os.system('make clean')
-dont_rm_files = ['lima.sip','limaconfig.py.in','configure.py','clean.py','LimaConvertor.h','lima_init_numpy.cpp']
+import glob
 
-for root,dirs,files in os.walk('.') :
-    for file_name in files :
-        if file_name not in dont_rm_files :
-            os.remove(os.path.join(root,file_name))
-    break
+modules = ['core', 'simulator', 'espia', 'frelon', 'maxipix']
+
+for mod in modules:
+    os.chdir(mod)
+    
+    if os.access('./Makefile',os.R_OK) :
+        os.system('make clean')
+#    dont_rm_files = ['lima.sip','limaconfig.py.in','configure.py','clean.py',
+#                     'LimaConvertor.h','lima_init_numpy.cpp', 'Makefile']
+
+    p = os.popen('svn propget svn:ignore .')
+    patterns = p.readlines()
+    if p.close() == 0:
+        for pat in patterns:
+            for f in glob.glob(pat.strip()):
+                print "execute: rm ", f
+#            os.remove(f)
+            
+#    for root,dirs,files in os.walk('.') :
+#        for file_name in files :
+#            if file_name not in dont_rm_files :
+#                os.remove(os.path.join(root,file_name))
+#        break
+
+    
+    os.chdir('..')
