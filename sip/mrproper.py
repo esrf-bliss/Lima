229c84a4d040b9e856f042b381269ebb1d1a27e6
ahoms <ahoms@45c4679d-1946-429d-baad-37912b19538b>
2010-06-14 09:18:40 +0000

    * Split lima library into core and independent camera modules
    git-svn-id: https://scm.blissgarden.org/svn/lima/trunk@257 45c4679d-1946-429d-baad-37912b19538b

--- sip/mrproper.py
+++ sip/mrproper.py
@@ -3,20 +3,37 @@
 import os
 import os.path
 import glob
+import shutil
 
 modules = ['core', 'simulator', 'espia', 'frelon', 'maxipix']
 
+p = os.popen('svn propget svn:ignore . 2> /dev/null')
+patterns = p.readlines()
+if not p.close():
+    for pat in patterns:
+        for f in glob.glob(pat.strip()):
+            if os.path.isdir(f):
+                print "execture: rmtree", f
+                #shutil.rmtree(f)
+            else:
+                print "execute: rm ", f
+                #os.remove(f)
+            
+
 for mod in modules:
-    os.chdir(mod)
+    try:
+        os.chdir(mod)
+    except OSError:
+        continue
     
     if os.access('./Makefile',os.R_OK) :
         os.system('make clean')
 #    dont_rm_files = ['lima.sip','limaconfig.py.in','configure.py','clean.py',
 #                     'LimaConvertor.h','lima_init_numpy.cpp', 'Makefile']
 
-    p = os.popen('svn propget svn:ignore .')
+    p = os.popen('svn propget svn:ignore . 2> /dev/null')
     patterns = p.readlines()
-    if p.close() == 0:
+    if not p.close():
         for pat in patterns:
             for f in glob.glob(pat.strip()):
                 print "execute: rm ", f
