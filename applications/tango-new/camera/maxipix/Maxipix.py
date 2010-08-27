7c412144faeb2b9f34709230334a784b03b150b7
laurent <laurent@45c4679d-1946-429d-baad-37912b19538b>
2010-08-27 11:33:07 +0000

    Added a new tango directory structure for better use of tags
    git-svn-id: https://scm.blissgarden.org/svn/lima/trunk@335 45c4679d-1946-429d-baad-37912b19538b

---
+++ applications/tango-new/camera/maxipix/Maxipix.py
@@ -0,0 +1,199 @@
+#=============================================================================
+#
+# file :        Pilatus.py
+#
+# description : Python source for the Maxipix and its commands. 
+#                The class is derived from Device. It represents the
+#                CORBA servant object which will be accessed from the
+#                network. All commands which can be executed on the
+#                Pilatus are implemented in this file.
+#
+# project :     TANGO Device Server
+#
+# copyleft :    European Synchrotron Radiation Facility
+#               BP 220, Grenoble 38043
+#               FRANCE
+#
+#=============================================================================
+#         (c) - Bliss - ESRF
+#=============================================================================
+#
+import PyTango
+import sys
+
+class Maxipix(PyTango.Device_4Impl):
+    def __init__(self,*args) :
+        PyTango.Device_4Impl.__init__(self,*args)
+        self.init_device()
+
+    def delete_device(self) :
+        pass
+
+    def init_device(self):
+        self.set_state(PyTango.DevState.ON)
+        self.get_device_properties(self.get_device_class())
+
+        #Init default Path
+        if self.config_path:
+            _MaxipixAcq.setPath(self.config_path)
+
+        #Load default config
+        if self.config_name:
+            _MaxipixAcq.loadConfig(self.config_name)
+        
+    ## @brief Read threshold noise of a maxipix chips
+    #
+    def read_threshold_noise(self,attr) :
+        dac = _MaxipixAcq.mpxDacs
+        thlNoises = dac.getThlNoise(0)
+
+        attr.set_value(thlNoises,len(thlNoises))
+        
+    ## @brief Write threshold noise of a maxipix chips
+    #
+    def write_threshold_noise(self,attr) :
+        data = []
+        attr.get_write_value(data)
+
+        dac = _MaxipixAcq.mpxDacs
+        dac.setThlNoise(0,*data)
+
+    ## @brief Read the global threshold
+    #
+    def read_threshold(self,attr) :
+        dac = _MaxipixAcq.mpxDacs
+        thl = dac.getThl()
+	if thl is None: thl = -1
+
+        attr.set_value(thl)
+
+    ## @brief Write the global threshold
+    #
+    def write_threshold(self,attr) :
+        data = []
+        attr.get_write_value(data)
+        
+        dac = _MaxipixAcq.mpxDacs
+        dac.setThl(data[0])
+	_MaxipixAcq.applyChipFsr(0)
+
+    ## @brief Read the energy step
+    #
+    # energy step is the coef which link the global threshold with energy
+    # threshold
+    # 
+    def read_energy_calibration(self,attr) :
+        dac = _MaxipixAcq.mpxDacs
+        values = dac.getECalibration()
+        
+        attr.set_value(values,len(values))
+        
+    ## @brief Write the energy step
+    #
+    def write_energy_calibration(self,attr) :
+        data = []
+        attr.get_write_value(data)
+
+        dac = _MaxipixAcq.mpxDacs
+        dat.setECalibration(*data)
+
+    ## @brief Read the energy threshold
+    #
+    # energy_threshold = energy_step * threshold (global)
+    def read_energy_threshold(self,attr) :
+        dac = _MaxipixAcq.mpxDacs
+        value = dac.getEThl()
+	if value is None: value = -1
+	
+        attr.set_value(value)
+
+    ## @brief Write the energy threshold
+    #
+    def write_energy_threshold(self,attr) :
+        data = []
+        attr.get_write_value(data)
+        
+        dac = _MaxipixAcq.mpxDacs
+        dac.setEThl(data[0])
+	_MaxipixAcq.applyChipFsr(0)
+        
+    ## @brief read the config name
+    #
+    def read_config_name(self,attr) :
+        cfg_name = ""
+        if self.config_name:
+            cfg_name = self.config_name
+        attr.set_value(cfg_name)
+
+    ## @brief Write the energy threshold
+    #
+    def write_config_name(self,attr) :
+        data = []
+        attr.get_write_value(data)
+        _MaxipixAcq.loadConfig(data[0])
+        self.config_name = data[0]
+
+
+class MaxipixClass(PyTango.DeviceClass):
+
+    class_property_list = {}
+
+    device_property_list = {
+        'config_path':
+        [PyTango.DevString,
+         "Path where configuration files are",[]],
+        'config_name':
+        [PyTango.DevString,
+         "The default configuration loaded",[]],
+        }
+
+    cmd_list = {}
+
+    attr_list = {
+        'threshold_noise':
+        [[PyTango.DevLong,
+          PyTango.SPECTRUM,
+          PyTango.READ_WRITE,5]],
+        'threshold':
+        [[PyTango.DevLong,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE]],
+        'energy_calibration':
+        [[PyTango.DevDouble,
+          PyTango.SPECTRUM,
+          PyTango.READ_WRITE,5]],
+        'energy_threshold':
+        [[PyTango.DevDouble,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE]],
+        'config_name':
+        [[PyTango.DevString,
+          PyTango.SCALAR,
+          PyTango.READ_WRITE]],
+        }
+
+    def __init__(self,name) :
+        PyTango.DeviceClass.__init__(self,name)
+        self.set_type(name)
+
+
+#----------------------------------------------------------------------------
+#                              Plugins
+#----------------------------------------------------------------------------
+from Lima.Maxipix.MpxAcq import MpxAcq
+
+_MaxipixAcq = None
+
+def get_control() :
+    global _MaxipixAcq
+    if _MaxipixAcq is None:
+        _MaxipixAcq = MpxAcq(0)
+    return _MaxipixAcq.getControl()
+
+def close_interface() :
+    global _MaxipixAcq
+    _MaxipixAcq = None
+    
+def get_tango_specific_class_n_device():
+    return MaxipixClass,Maxipix
+
