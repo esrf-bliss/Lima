ed07b70230fb117d91a5cf096f5f8eb6b628dd24
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2013-02-15 11:38:22 +0100

    Fixed a window warning

--- common/src/ConfigUtils.cpp
+++ common/src/ConfigUtils.cpp
@@ -19,6 +19,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
 //###########################################################################
+#ifdef WIN32
+#pragma warning(disable : 4290)
+#endif
 #include <libconfig.h++>
 
 #include "Exceptions.h"
