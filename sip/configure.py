90560c6f80f317968a90723d5de669738f6b4088
Laurent Claustre <claustre@esrf.fr>
2015-02-27 14:27:14 +0100

    Updates for roperscientific

--- sip/configure.py
+++ sip/configure.py
@@ -42,10 +42,8 @@ modules = [('core',             ['common', 'hardware', 'control']),
            ('prosilica',        [os.path.join('camera','prosilica')]),
            ('ueye',             [os.path.join('camera','ueye')]),
            ('roperscientific',  [os.path.join('camera','roperscientific')]),
-           ('adsc',  		[os.path.join('camera','adsc')]),
-           ('merlin',		[os.path.join('camera','merlin')]),
+           ('adsc',             [os.path.join('camera','adsc')]),
            ('mythen',           [os.path.join('camera','mythen')]),
-           ('mythen3',		[os.path.join('camera','mythen3')]),
            ('perkinelmer',      [os.path.join('camera','perkinelmer')]),
            ('andor',            [os.path.join('camera','andor')]),
            ('andor3',           [os.path.join('camera','andor3')]),
@@ -182,8 +180,7 @@ def main():
         elif(modName == 'xpad'):
             extraIncludes += ['../../third-party/yat/include','/home/xpix_user/PCI_VALIDATED/trunk/sw/xpci_lib']
         elif(modName == 'xspress3'):
-            extraIncludes += ['../../third-party/hdf5/c++/src']
-            extra_cxxflags += ['-DSIPCOMPILATION']
+            extraIncludes += ['../../third-party/hdf5/include']
         elif(modName == 'pco'):
             extraIncludes += ['R:/bliss/projects/LIMA/package/WIN32/PCO/sdkPco/include']
         elif(modName == 'marccd'):
@@ -197,6 +194,9 @@ def main():
         elif(modName == 'aviex'):
             extra_cxxflags += ['-DOS_UNIX']
         extraIncludes += findModuleIncludes(modName)
+	if (modName == 'roperscientific'):
+            extraIncludes.remove('../../camera/roperscientific/sdk/msvc/include')
+            print extraIncludes
         
         sipFile = open(sipFileName,"a")
         sipFile.write('\n')
