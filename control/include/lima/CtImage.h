48ee2d57a87586428827ca8fa05c8c0be15e4d7e
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2015-11-05 12:37:31 +0100

    image: reevaluate software roi when binning change

--- control/include/lima/CtImage.h
+++ control/include/lima/CtImage.h
@@ -186,6 +186,7 @@ class LIMACORE_API CtImage {
     private:
 	void _setMaxImage(const Size &size, ImageType type);
 	void _setHSRoi(const Roi &roi);
+	void _completeWithSoftRoi(Roi roi_set,Roi hw_roi);
 	void _setHSBin(const Bin &bin);
 	void _setHSFlip(const Flip &flip);
 	void _resetFlip();
