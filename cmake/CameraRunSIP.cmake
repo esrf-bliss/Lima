ec4c3e5a959c83d8bc41b1fdc3f0295e6243b109
Aitor Henry <aitor@lid00limace.esrf.fr>
2017-06-16 13:39:27 +0200

    Adding CameraRunSIP.cmake file to repo. this file is use by every camera when compile_sip is activate. Updating submodules, also adding license to CMakeLists.txt Adding new cameras (hamamatsu, hexitec and photonicscience).

---
+++ cmake/CameraRunSIP.cmake
@@ -0,0 +1,31 @@
+SET(INCLUDES)
+FILE(GLOB sipfiles RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}/sip" "${CMAKE_CURRENT_SOURCE_DIR}/sip/*.sip")
+FOREACH(sipfile ${sipfiles})
+	SET(INCLUDES 
+		"${INCLUDES} 
+        %Include ${sipfile}")
+ENDFOREACH()
+
+if(SIP_VERSION_STR VERSION_LESS "4.12")
+	configure_file(${CMAKE_SOURCE_DIR}/sip/limamodules_before_4_12.sip.in sip/lima${NAME}.sip)
+else()
+	CONFIGURE_FILE(${CMAKE_SOURCE_DIR}/sip/limamodules.sip.in sip/lima${NAME}.sip)
+endif()
+SET(SIP_CONCAT_PARTS 1)
+SET(SIP_INCLUDES ${SIP_INCLUDES}
+    "${CMAKE_SOURCE_DIR}/third-party/Processlib/sip"
+    "${CMAKE_BINARY_DIR}/sip/core"
+    "${CMAKE_SOURCE_DIR}/third-party/Processlib/tasks/sip"
+    "${CMAKE_SOURCE_DIR}/common/sip"
+    "${CMAKE_SOURCE_DIR}/hardware/sip"
+    "${CMAKE_SOURCE_DIR}/control/sip"
+    "${CMAKE_SOURCE_DIR}/control/software_operation/sip"
+    "${CMAKE_CURRENT_SOURCE_DIR}/sip")
+set(SIP_DISABLE_FEATURES WITH_CONFIG)
+set(SIP_CHECK_EXC ON)
+ADD_SIP_PYTHON_MODULE(lima${NAME} ${CMAKE_CURRENT_BINARY_DIR}/sip/lima${NAME}.sip)
+TARGET_INCLUDE_DIRECTORIES(python_module_lima${NAME} PRIVATE
+                          ${PYTHON_INCLUDE_DIRS}
+                          "${CMAKE_SOURCE_DIR}/sip"
+                          "${CMAKE_SOURCE_DIR}/sip/core")
+TARGET_LINK_LIBRARIES(python_module_lima${NAME} lima${NAME})
