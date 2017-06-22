b7975d3aa497f0daa7fada39ee76aba3ab847149
Aitor Henry <aitor@lid00limace.esrf.fr>
2017-06-22 13:28:39 +0200

    Added test possibility for cameras (using ctest). Also modified few stuff with sip.

--- cmake/CameraRunSIP.cmake
+++ cmake/CameraRunSIP.cmake
@@ -2,9 +2,13 @@ SET(INCLUDES)
 FILE(GLOB sipfiles RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}/sip" "${CMAKE_CURRENT_SOURCE_DIR}/sip/*.sip")
 FOREACH(sipfile ${sipfiles})
 	SET(INCLUDES 
-		"${INCLUDES} 
-        %Include ${sipfile}")
+	"${INCLUDES}
+%Include ${sipfile}")
 ENDFOREACH()
+
+SET(IMPORTS 
+"${IMPORTS}
+%Import limacore.sip")
 
 if(SIP_VERSION_STR VERSION_LESS "4.12")
 	configure_file(${CMAKE_SOURCE_DIR}/sip/limamodules_before_4_12.sip.in sip/lima${NAME}.sip)
