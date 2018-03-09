0118f65df2d6e45a867dfc8a629833ee56449538
Samuel Debionne <samuel.debionne@esrf.fr>
2018-03-09 10:25:28 +0100

    Remove the cleanup of the build directory
    The scripts may be executed from a working directory that is not the root of the lima project...

--- install.bat
+++ install.bat
@@ -6,27 +6,22 @@ rem   Copyright (C) : 2009-2017
 rem   European Synchrotron Radiation Facility
 rem   BP 220, Grenoble 38043
 rem   FRANCE
-rem  
+rem
 rem   Contact: lima@esrf.fr
-rem  
+rem
 rem   This is free software; you can redistribute it and/or modify
 rem   it under the terms of the GNU General Public License as published by
 rem   the Free Software Foundation; either version 3 of the License, or
 rem   (at your option) any later version.
-rem 
+rem
 rem   This software is distributed in the hope that it will be useful,
 rem   but WITHOUT ANY WARRANTY; without even the implied warranty of
 rem   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 rem   GNU General Public License for more details.
-rem 
+rem
 rem   You should have received a copy of the GNU General Public License
 rem   along with this program; if not, see <http://www.gnu.org/licenses/>.
 rem ###########################################################################
-
-if exist "build" (
-    rmdir /S /Q build
-)
-mkdir build
 
 cd scripts/
 if not exist config.txt (
--- install.sh
+++ install.sh
@@ -6,35 +6,22 @@
 #  European Synchrotron Radiation Facility
 #  BP 220, Grenoble 38043
 #  FRANCE
-# 
+#
 #  Contact: lima@esrf.fr
-# 
+#
 #  This is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
-# 
+#
 #  This software is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-# 
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, see <http://www.gnu.org/licenses/>.
 ############################################################################
-
-if [ -d "build/" ]; then
-	cd build/
-	#We test if the build directory is empty or not
-	empty_dir=$(ls -l)
-	if [ "$build_dir" != "total 0" ]; then
-		rm -rf *
-	fi
-else
-	mkdir build/
-	cd build/
-fi
-cd ..
 
 cd scripts/
 if [ ! -f "config.txt" ]; then
@@ -45,4 +32,3 @@ cd ..
 python scripts/bootstrap.py $@
 
 exit $?
-
