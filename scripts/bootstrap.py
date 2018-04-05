c6a7e6a45f3d3508c244f07973330abd3de0759f
Alejandro Homs Puron <ahoms@esrf.fr>
2018-04-05 12:37:10 +0200

    Install: improve bootstrap.py, Linux and Windows target use same CMake build:
    * Better command line option management:
      --no-xxxx is equivalent to --xxxx=no
    * Installation is now optional, the default is 'no',
      if --install-prefix is specified, then --install will be assumed

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -29,7 +29,7 @@ if OS_TYPE not in ['Linux', 'Windows']:
 	sys.exit('Platform not supported: ' + OS_TYPE)
 
 
-class Options:
+class Config:
 
 	defaults = {
 		'git': False,
@@ -39,19 +39,13 @@ class Options:
 		'build-prefix': 'build',
 		'build-type': ('RelWithDebInfo' if OS_TYPE == 'Linux' 
 			       else 'Release'),
+		'install': False,
 		'install-prefix': '',
 		'install-python-prefix': '',
 	}
 
-	cmd_2_cmake_map = [
-		('build-type', 'cmake-build-type'),
-		('install-prefix', 'cmake-install-prefix'),
-		('install-python-prefix', 'python-site-packages-dir'),
-		('find-root-path', 'cmake-find-root-path')
-	]
-
 	def __init__(self, argv=None):
-		self.opts = {}
+		self.cmd_opts = {}
 		self.extra_opts = []
 		self.config_opts = None
 		self.cmake_opts = None
@@ -65,31 +59,36 @@ class Options:
 			if arg in ['--help', '-help', '-h', '-?']:
 				print_help()
 			for opt in self.defaults:
+				if arg == '--no-%s' % opt:
+					arg = '--%s=no' % opt
 				prefix = '--%s' % opt
 				if arg == prefix:
-					self.set(opt, True)
+					self.set_cmd(opt, True)
 					break
-				elif arg.startswith(prefix + '='):
-					self.set(opt, arg[len(prefix) + 1:])
+				prefix += '='
+				if arg.startswith(prefix):
+					val = arg[len(prefix):]
+					val_map = {'yes': True, 'no': False}
+					val = val_map.get(val.lower(), val)
+					self.set_cmd(opt, val)
 					break
 			else:
 				self.add_extra(arg)
 
-		src = self.get('source-prefix')
+		cwd = os.getcwd()
 		if not self.get('source-prefix'):
-			src = os.getcwd()
-			self.set('source-prefix', src)
+			self.set_cmd('source-prefix', cwd)
 		for opt in ['config-file', 'build-prefix']:
 			p = self.get(opt)
-			if not os.path.isabs(p):
-				self.set(opt, os.path.join(src, p))
+			if p and not os.path.isabs(p):
+				self.set_cmd(opt, os.path.join(cwd, p))
 
-	def set(self, x, v):
-		self.opts[x] = v
+	def set_cmd(self, x, v):
+		self.cmd_opts[x] = v
 
 	def get(self, x, check_defaults=True):
-		if x in self.opts:
-			return self.opts[x]
+		if x in self.cmd_opts:
+			return self.cmd_opts[x]
 		return self.defaults[x] if check_defaults else ''
 
 	def add_extra(self, x):
@@ -98,15 +97,16 @@ class Options:
 	def get_git_options(self):
 		return self.extra_opts
 
-	def get_cmd_opts(self, check_defaults=False):
+	def get_cmd_options(self, check_defaults=False):
 		opts = dict(self.defaults) if check_defaults else {}
-		opts.update(self.opts)
+		opts.update(self.cmd_opts)
 		for arg in self.extra_opts:
-			for p, d in [("limacamera", "camera"), 
-				     ("lima-enable", "third-party")]:
-				if arg.startswith(d + '/'):
-					arg = p + '-' + arg[len(d) + 1:]
-			opts[arg] = str(1)
+			for oprefix, sdir in [("limacamera", "camera"), 
+					      ("lima-enable", "third-party")]:
+				sdir += '/'
+				if arg.startswith(sdir):
+					arg = oprefix + '-' + arg[len(sdir):]
+			opts[arg] = True
 		return opts
 
 	def read_config(self):
@@ -119,46 +119,21 @@ class Options:
 					continue
 				opt, val = line.split('=')
 				opt = self.from_underscore(opt).lower()
+				val = int(val) if val.isdigit() else val
 				self.config_opts.append((opt, val))
 
-	# return options in config file activated (=1) if passed as arguments,
-	# and also those not specified as empty (=) or disabled (=0) in file
-	def get_cmake_opts(self):
-		cmd_opts = self.get_cmd_opts()
+	def get_config_options(self):
 		if self.config_opts is None:
 			self.read_config()
+		return self.config_opts
 
-		def is_active(v):
-			return val and (val != str(0))
+	def get_cmake_options(self):
+		return CMakeOptions(self)
 
-		cmake_opts = []
-		for opt, val in self.config_opts:
-			for cmd_opt, cmd_val in cmd_opts.items():
-				if cmd_opt == opt:
-					val = cmd_val
-					break
-				# arg-passed option must match the end
-				# of opt a nd must be preceeded by 
-				# the '_' separator
-				t = opt.split(cmd_opt)
-				if ((len(t) == 2) and 
-				    (t[0][-1] == '_') and not t[1]):
-					val = cmd_val
-					break
-			if is_active(val):
-				cmake_opts.append((opt, val))
-
-		for cmd_key, cmake_key in self.cmd_2_cmake_map:
-			val = self.get(cmd_key)
-			if is_active(val) and cmake_key not in dict(cmake_opts):
-				cmake_opts.append((cmake_key, val))
-
-		return CMakeOptions(self.get('source-prefix'), cmake_opts)
-
-	def get_cmake_cmd_line(self):
-		if self.cmake_opts is None:
-			self.cmake_opts = self.get_cmake_opts()
-		return self.cmake_opts.get_cmd_line_opts()
+	def is_install_required(self):
+		cmd_opts = self.get_cmd_options()
+		install_prefix = cmd_opts.get('install-prefix', '')
+		return cmd_opts.get('install', install_prefix != '')
 
 	@staticmethod
 	def print_help():
@@ -174,15 +149,54 @@ class Options:
 	def from_underscore(x):
 		return x.replace('_', '-')
 
-
 class CMakeOptions:
 
-	def __init__(self, source_prefix, opts):
-		self.source_prefix = source_prefix
-		self.opts = opts
+	cmd_2_cmake_map = [
+		('build-type', 'cmake-build-type'),
+		('install-prefix', 'cmake-install-prefix'),
+		('install-python-prefix', 'python-site-packages-dir'),
+		('find-root-path', 'cmake-find-root-path')
+	]
+
+	def __init__(self, cfg):
+		self.cfg = cfg
+
+
+	# return options in config file activated (=1) if passed as arguments,
+	# and also those not specified as empty (=) or disabled (=0|no) in file
+	def get_configure_options(self):
+		cmd_opts = self.cfg.get_cmd_options()
+		config_opts = self.cfg.get_config_options()
+
+		def is_active(v):
+			if type(val) in [bool, int]:
+				return val
+			return val and (val.lower() not in [str(0), 'no'])
+
+		cmake_opts = []
+		for opt, val in config_opts:
+			for cmd_opt, cmd_val in cmd_opts.items():
+				if cmd_opt == opt:
+					val = cmd_val
+					break
+				# arg-passed option must match the end
+				# of opt a nd must be preceeded by 
+				# the '_' separator
+				t = opt.split(cmd_opt)
+				if ((len(t) == 2) and 
+				    (t[0][-1] == '-') and not t[1]):
+					val = cmd_val
+					break
+			if is_active(val):
+				cmake_opts.append((opt, val))
+
+		for cmd_key, cmake_key in self.cmd_2_cmake_map:
+			val = self.cfg.get(cmd_key)
+			if is_active(val) and cmake_key not in dict(cmake_opts):
+				cmake_opts.append((cmake_key, val))
 
 		if OS_TYPE == 'Linux':
-			self.cmake_gen = 'Unix Makefiles'
+			cmake_gen = 'Unix Makefiles'
 		elif OS_TYPE == 'Windows':
 			# for windows check compat between installed python 
 			# and mandatory vc++ compiler
@@ -201,20 +215,41 @@ class CMakeOptions:
 
 			print ('Found Python ', sys.version)
 			print ('Used compiler: ', win_compiler)
-			self.cmake_gen = win_compiler
+			cmake_gen = win_compiler
 
-	def get_cmd_line_opts(self):
-		opts = [self.source_prefix, '-G"%s"' % self.cmake_gen]
-		opts += map(self.cmd_opt, self.opts)
-		return ' '.join(opts)
+		source_prefix = self.cfg.get('source-prefix')
+		opts = [source_prefix, '-G"%s"' % cmake_gen]
+		opts += map(self.cmd_option, cmake_opts)
+		return self.get_cmd_line_from_options(opts)
+
+	def get_build_options(self):
+		opts = ['--build .']
+		if OS_TYPE == 'Linux':
+			nb_jobs = multiprocessing.cpu_count() + 1
+			opts += ['--', '-j %d' % nb_jobs]
+		if OS_TYPE == 'Windows':
+			opts += ['--config %s' %  self.cfg.get('build-type')]
+		return self.get_cmd_line_from_options(opts)
+
+	def get_install_options(self):
+		opts = ['--build .', '--target install'] 
+		return self.get_cmd_line_from_options(opts)
 
 	@staticmethod
-	def cmd_opt(opt_val):
+	def get_cmd_line_from_options(opts):
+		return ' '.join(['cmake'] + opts)
+
+	@staticmethod
+	def cmd_option(opt_val):
 		o, v = opt_val
 		def quoted(x):
+			if type(x) is bool:
+				x = int(x)
+			if type(x) is not str:
+				x = str(x)
 			return (('"%s"' % x) 
 				if ' ' in x and not x.startswith('"') else x)
-		return '-D%s=%s' % (Options.to_underscore(o).upper(), quoted(v))
+		return '-D%s=%s' % (Config.to_underscore(o).upper(), quoted(v))
 
 
 class GitHelper:
@@ -267,93 +302,61 @@ class GitHelper:
 			for submod in submod_list:
 				action = 'init ' + submod
 				ret = os.system('git submodule ' + action)
-				if re != 0:
+				if ret != 0:
 					raise Exception('Could not init')
 				action = 'update --recursive ' + submod
 				ret = os.system('git submodule ' + action)
-				if re != 0:
+				if ret != 0:
 					raise Exception('Could not update')
 		except Exception as e:
 			sys.exit('Problem with submodule %s: %s' % (action, e))
 
 
-def go_build(opts):
-	build_prefix = opts.get('build-prefix')
+def exec_cmd(cmd, exc_msg):
+	print('Executing: ' + cmd)
+	sys.stdout.flush()
+	ret = os.system(cmd)
+	if ret != 0:
+		raise Exception(exc_msg)
+
+
+def build_install_lima(cfg):
+	build_prefix = cfg.get('build-prefix')
 	if not os.path.exists(build_prefix):
 		os.mkdir(build_prefix)
 	os.chdir(build_prefix)
 
+	cmake_opts = cfg.get_cmake_options()
+	cmake_cmd = cmake_opts.get_configure_options()
+	exec_cmd(cmake_cmd, ('Something is wrong in CMake environment. ' +
+			     'Make sure your configuration is good.'))
 
-def build_install_lima_linux(opts):
-	go_build(opts)
-	try:
-		cmake_opts = opts.get_cmake_cmd_line()
-		print(cmake_opts)
-		sys.stdout.flush()
+	cmake_cmd = cmake_opts.get_build_options()
+	exec_cmd(cmake_cmd, ('CMake could not build Lima. ' + 
+			     'Pleae contact lima@esrf.fr for help.'))
 
-		action = 'configuration'
-                ret = os.system('cmake ' + cmake_opts)
-                if ret != 0:
-                        raise Exception('Something is wrong in your ' +
-					'CMake environement. Make sure ' + 
-					'your configuration is good.')
+	if not cfg.is_install_required():
+		return
 
-		action = 'compilation'
-		nb_cpus = multiprocessing.cpu_count()
-                ret = os.system('make -j%d' % (nb_cpus + 1))
-                if ret != 0:
-                        raise Exception('CMake could not build Lima. ' + 
-					'Contact lima@esrf.fr for help.')
+	cmake_cmd = cmake_opts.get_install_options()
+	exec_cmd(cmake_cmd, ('CMake could not install libraries. ' + 
+			     'Make sure you have necessary rights.'))
 
-		action = 'installation'
-                ret = os.system('make install')
-                if ret != 0 :
-                        raise Exception('CMake could not install libraries. ' + 
-					'Make sure you have necessary rights.')
-	except Exception as e:
-		sys.exit('Problem in CMake %s: %s' % (action, e))
-
-
-def build_install_lima_windows(opts):
-	go_build(opts)
-	try :
-		cmake_opts = opts.get_cmake_cmd_line()
-		print(cmake_opts)
-		sys.stdout.flush()
-
-		action = 'configuration'
-                ret = os.system('cmake ' + cmake_opts)
-		if ret != 0:
-			raise Exception('Something went wrong in the CMake ' + 
-					'preparation. Make sure ' + 
-					'your configuration is good.')
-
-		action = 'compilation or installation'
-		cmake_compile_opts = ['--build .', '--target install',
-				      '--config %s' % opts.get('build-type')]
-		cmake_cmd_line_opts = ' '.join(cmake_compile_opts)
-		ret = os.system('cmake ' + cmake_cmd_line_opts)
-		if ret != 0:
-			raise Exception('CMake could not build or install ' + 
-					'libraries. Contact lima@esrf.fr ' + 
-					'for help.')
-	except Exception as e:
-		sys.exit('Problem in CMake %s: %s' % (action, e))
-			
 
 def main():
-	opts = Options(sys.argv[1:])
+	cfg = Config(sys.argv[1:])
 
 	# No git option under windows for obvious reasons.
-	if OS_TYPE == 'Linux' and opts.get('git'):
-		git_opts = opts.get_git_options()
+	if OS_TYPE == 'Linux' and cfg.get('git'):
+		git_opts = cfg.get_git_options()
 		git = GitHelper(git_opts)
 		git.check_submodules()
 
-	if OS_TYPE == 'Linux':
-		build_install_lima_linux(opts)
-	elif OS_TYPE == 'Windows':
-		build_install_lima_windows(opts)
+	try:
+		build_install_lima(cfg)
+	except Exception as e:
+		sys.exit('Problem building/installing Lima: %s' % e)
+
 
 
 if __name__ == '__main__':
