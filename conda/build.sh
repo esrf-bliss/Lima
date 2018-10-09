413783e2eb6c6986afa19d22b6b2e9caf669b3c3
Samuel Debionne <samuel.debionne@esrf.fr>
2018-10-09 11:02:53 +0200

    Stop building cameras as it's to much a hassle to maintain the CI and cameras have their own CIs.

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python -V
-./install.sh --install-prefix=$PREFIX --install-python-prefix=$SP_DIR --find-root-path=$PREFIX hdf5 hdf5-bs edfgz edflz4 cbf tiff python pytango-server basler pilatus v4l2 espia maxipix frelon andor andor3 prosilica
+./install.sh --install-prefix=$PREFIX --install-python-prefix=$SP_DIR --find-root-path=$PREFIX hdf5 hdf5-bs edfgz edflz4 cbf tiff python pytango-server
 
 # Conda requires the library to be installed in /lib not /lib64 regardeless of the HFS standard.
 mv -f $PREFIX/lib64/* $PREFIX/lib
