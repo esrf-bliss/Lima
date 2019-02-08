e7afd2a586c07ba39ffa16c508e818f7cf143d84
Samuel Debionne <samuel.debionne@esrf.fr>
2019-02-08 10:56:40 +0100

    Update copyright in doc

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Lima'
-copyright = u'2016, Lima Team'
+copyright = u'2019, Lima Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
