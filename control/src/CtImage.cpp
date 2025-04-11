60630cb66dd3aac359c277430da004fdef0a2d97
Valentin Valls <valentin.valls@esrf.fr>
2025-04-11 14:07:43 +0200

    Handle the hard/soft mode for roi

--- control/src/CtImage.cpp
+++ control/src/CtImage.cpp
@@ -869,7 +869,7 @@ void CtImage::_setHSBin(const Bin &bin, BinMode bin_mode)
 	}
 	user_roi = user_roi.getUnbinned(SwapDimIfRotated(user_rot, user_bin));
 	user_roi = user_roi.getBinned(SwapDimIfRotated(user_rot, bin));
-	_completeWithSoftRoi(user_roi, m_hw->getRealRoi());
+	setRoi(user_roi);
 }
 
 
