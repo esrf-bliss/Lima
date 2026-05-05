bc6470a0e720c496e0b86c0cbcf60fe569ab353e
pautran <pierre-olivier.autran@esrf.fr>
2026-05-05 15:12:57 +0200

    cmake: Support environment overrides for JP2K libraries

--- Saving.cmake
+++ Saving.cmake
@@ -103,9 +103,22 @@ if(LIMA_ENABLE_HDF5)
   endif()
 
   if(LIMA_ENABLE_HDF5_JP2K)
+    set(OPENJPEG_ROOT "" CACHE PATH "Path to OpenJPEG root directory")
+    if(DEFINED ENV{OPENJPEG_ROOT})
+      set(OPENJPEG_ROOT "$ENV{OPENJPEG_ROOT}" CACHE PATH "Path to OpenJPEG root directory" FORCE)
+    endif()
+    if(DEFINED ENV{OPENJPEG_INCLUDE_DIR})
+      set(OPENJPEG_INCLUDE_DIR "$ENV{OPENJPEG_INCLUDE_DIR}" CACHE PATH "Path to OpenJPEG include directory" FORCE)
+    endif()
+    if(DEFINED ENV{OPENJPEG_LIBRARY})
+      set(OPENJPEG_LIBRARY "$ENV{OPENJPEG_LIBRARY}" CACHE FILEPATH "Path to OpenJPEG library" FORCE)
+    endif()
+
     find_path(OPENJPEG_INCLUDE_DIR openjpeg.h
+      HINTS "${OPENJPEG_ROOT}/include" "${OPENJPEG_ROOT}"
       PATH_SUFFIXES openjpeg-2.5 openjpeg-2.4 openjpeg-2.3 openjpeg-2.2 openjpeg-2.1)
-    find_library(OPENJPEG_LIBRARY openjp2)
+    find_library(OPENJPEG_LIBRARY openjp2
+      HINTS "${OPENJPEG_ROOT}/lib" "${OPENJPEG_ROOT}/lib64" "${OPENJPEG_ROOT}")
     if(NOT OPENJPEG_INCLUDE_DIR OR NOT OPENJPEG_LIBRARY)
       message(FATAL_ERROR "OpenJPEG library not found, please install openjpeg development files or disable LIMA_ENABLE_HDF5_JP2K")
     endif()
@@ -114,15 +127,37 @@ if(LIMA_ENABLE_HDF5)
 
     list(APPEND saving_definitions -DWITH_JP2K_COMPRESSION)
 
-    set(KAKADU_ROOT "$ENV{HOME}/Bliss/v8_6_2-02252E" CACHE PATH "Path to Kakadu root directory")
+    set(KAKADU_ROOT "" CACHE PATH "Path to Kakadu root directory")
+    set(KAKADU_LIBRARY_DIR "" CACHE PATH "Path to Kakadu library directory")
+    if(DEFINED ENV{KAKADU_ROOT})
+      set(KAKADU_ROOT "$ENV{KAKADU_ROOT}" CACHE PATH "Path to Kakadu root directory" FORCE)
+    endif()
+    if(DEFINED ENV{KAKADU_LIBRARY_DIR})
+      set(KAKADU_LIBRARY_DIR "$ENV{KAKADU_LIBRARY_DIR}" CACHE PATH "Path to Kakadu library directory" FORCE)
+    endif()
+    if(DEFINED ENV{KAKADU_CORESYS_INCLUDE_DIR})
+      set(KAKADU_CORESYS_INCLUDE_DIR "$ENV{KAKADU_CORESYS_INCLUDE_DIR}" CACHE PATH "Path to Kakadu coresys include directory" FORCE)
+    endif()
+    if(DEFINED ENV{KAKADU_SUPPORT_INCLUDE_DIR})
+      set(KAKADU_SUPPORT_INCLUDE_DIR "$ENV{KAKADU_SUPPORT_INCLUDE_DIR}" CACHE PATH "Path to Kakadu support include directory" FORCE)
+    endif()
+    if(DEFINED ENV{KAKADU_AUX_LIBRARY})
+      set(KAKADU_AUX_LIBRARY "$ENV{KAKADU_AUX_LIBRARY}" CACHE FILEPATH "Path to Kakadu auxiliary library" FORCE)
+    endif()
+    if(DEFINED ENV{KAKADU_CORE_LIBRARY})
+      set(KAKADU_CORE_LIBRARY "$ENV{KAKADU_CORE_LIBRARY}" CACHE FILEPATH "Path to Kakadu core library" FORCE)
+    endif()
+
     find_path(KAKADU_CORESYS_INCLUDE_DIR kdu_compressed.h
-      PATHS "${KAKADU_ROOT}/coresys/common" NO_DEFAULT_PATH)
+      HINTS "${KAKADU_ROOT}/coresys/common")
     find_path(KAKADU_SUPPORT_INCLUDE_DIR kdu_stripe_compressor.h
-      PATHS "${KAKADU_ROOT}/apps/support" NO_DEFAULT_PATH)
+      HINTS "${KAKADU_ROOT}/apps/support")
     find_library(KAKADU_AUX_LIBRARY kdu_a86R
-      PATHS "${KAKADU_ROOT}/lib/Linux-x86-64-gcc" NO_DEFAULT_PATH)
+      HINTS "${KAKADU_LIBRARY_DIR}" "${KAKADU_ROOT}/lib/Linux-x86-64-gcc"
+            "${KAKADU_ROOT}/lib")
     find_library(KAKADU_CORE_LIBRARY kdu_v86R
-      PATHS "${KAKADU_ROOT}/lib/Linux-x86-64-gcc" NO_DEFAULT_PATH)
+      HINTS "${KAKADU_LIBRARY_DIR}" "${KAKADU_ROOT}/lib/Linux-x86-64-gcc"
+            "${KAKADU_ROOT}/lib")
     if(KAKADU_CORESYS_INCLUDE_DIR AND KAKADU_SUPPORT_INCLUDE_DIR AND
        KAKADU_AUX_LIBRARY AND KAKADU_CORE_LIBRARY)
       list(APPEND saving_definitions -DWITH_KAKADU_JP2K)
@@ -131,7 +166,7 @@ if(LIMA_ENABLE_HDF5)
       list(APPEND saving_libs ${KAKADU_AUX_LIBRARY} ${KAKADU_CORE_LIBRARY}
                               dl)
     else()
-      message(STATUS "Kakadu JP2K support disabled: set KAKADU_ROOT to a built Kakadu tree")
+      message(STATUS "Kakadu JP2K support disabled: set KAKADU_ROOT or Kakadu include/library paths")
     endif()
   endif()
 endif()
